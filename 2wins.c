e0e9cb907c89f4432655099983389936ad25e242
Kyle Glaws <kyle.glaws@gmail.com>
2020-03-29 00:06:37 -0400

    have input and output windows working. Need to fix printing over border issue, and add new line after printing to output window.

--- 2wins.c
+++ 2wins.c
@@ -2,58 +2,89 @@
 #include <ncurses.h>
 
 
+static WINDOW *top = NULL;
+static WINDOW *bot = NULL;
+
+
+void init_ui()
+{
+  /* enter curses mode */
+  initscr();
+  //raw();
+  refresh();
+
+  /* calculate screen size */
+  int height, width;
+  getmaxyx(stdscr, height, width);
+  int htop = ((height / 3) * 2) - 2;
+  int hbot = (height / 3) - 2;
+  width -= 2;
+
+  /* create top window */
+  top = newwin(htop, width, 1, 1);
+  box(top, 0, 0);
+  wrefresh(top);
+
+  /* create bottom window */
+  bot = newwin(hbot, width, htop + 2, 1);
+  box(bot, 0, 0);
+  wrefresh(bot);
+}
+
+
+void window_resize()
+{
+  /* For some reason, things work better if you refresh 
+   * stdscr BEFORE making these changes.
+   */
+  refresh(); 
+
+  /* calculate new screen size */
+  int height, width;
+  getmaxyx(stdscr, height, width);
+  int htop = ((height / 3) * 2) - 2;
+  int hbot = (height / 3) - 2;
+  width -= 2;
+
+  /* clear old top window border */
+  wborder(top, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+  wrefresh(top);
+
+  /* resize and draw new border */
+  wresize(top, htop, width);
+  box(top, 0, 0);
+  wrefresh(top);
+
+  /* clear old bot window border */
+  wborder(bot, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+  wrefresh(bot);
+
+  /* resize, move bot window up or down (relative to height of top window),
+   * and draw new border
+   */
+  wresize(bot, hbot, width);
+  mvwin(bot, htop + 2, 1);
+  box(bot, 0, 0);
+  wrefresh(bot);
+}
+
+
 int main()
 {
-  int height, width;
-  WINDOW *top, *bot;
+  init_ui();
 
-  initscr();
-  raw();
+  char msg[64];
 
-  getmaxyx(stdscr, height, width);
-
-  top = newwin((height/2)-2, width-2, 1, 1);
-  box(top, 0, 0);
-  //wborder(top, ' ', '|', '-', '-', '.', '.', '.', '.');
-
-  bot = newwin((height/2)-2, width-2, (height/2)+1, 1);
-  box(bot, 0, 0);
-  //wborder(bot, '|', '|', '-', '-', '.', '.', '.', '.');
-
-  wrefresh(top);
-  wrefresh(bot);
-  //refresh();
-
-  int ch;
-  do
+  while (1)
   {
-    ch = getch();
+    refresh();
 
-    if (ch == KEY_RESIZE)
-    {
-      getmaxyx(stdscr, height, width);
+    wgetstr(bot, msg);
+    wrefresh(bot);
 
-      // clear old borders
-      wborder(top, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-      wborder(bot, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-
-      // resize windows
-      wresize(top, (height/2)-2, width-2);
-      wresize(bot, (height/2)-2, width-2);
-
-      // adjust bottom window position
-      mvwin(bot, (height/2)+1, 1);
-
-      // redraw borders
-      box(top, 0, 0);
-      box(bot, 0, 0);
-
-      wrefresh(top);
-      wrefresh(bot);
-      //refresh();
-    }
-
-  } while(ch != 'Q');
+    wprintw(top, msg);
+    wrefresh(top);
+  }
 
   endwin();
 }
