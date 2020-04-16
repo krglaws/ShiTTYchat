4a955700f45dba650d76a9da8804adce2fbb919b
Kyle Glaws <kyle.glaws@gmail.com>
2020-04-16 13:33:16 -0400

    almost done with main client module

--- client/client.c
+++ client/client.c
@@ -3,6 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+
+#include <rsa.h>
+#include <comm.h>
 
 
 int main(int argc, char **argv)
@@ -45,12 +49,25 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
+	/* generate rsa keys */
+	rsa_key_t pubkey, privkey;
+	rsa_init(pubkey, privkey, 1024, 62);
+
 	/* connect to server */
-	
+	int sock;
+	if ((sock = connect_to_server(ip, uname, port)) == -1)
+	{
+		fprintf(stderr, "main(): failed on call to connect_to_server()\n");
+		rsa_clear(pubkey);
+		rsa_clear(privkey);
+		return -1;
+	}
+
+	/* perform handshake */
 }
 
 
-bool valid_uname(char* uname)
+bool valid_uname(char *uname)
 {
 	int len = strlen(uname);
 	if (len > UNAMELEN)
@@ -73,11 +90,11 @@ bool valid_uname(char* uname)
 }
 
 
-bool valid_ip(char* ip)
+bool valid_ip(char *ip)
 {
 	struct sockaddr_in sa;
 
-	if (ip && inet_pton(AF_INET, ip, &(sa.sin_addr)) == 0)
+	if (!ip || inet_pton(AF_INET, ip, &(sa.sin_addr)) == 0)
 	{
 		fprintf(stderr, "invalid ip address\n");
 		return false;
@@ -87,7 +104,7 @@ bool valid_ip(char* ip)
 }
 
 
-bool valid_port(char* port)
+bool valid_port(char *port)
 {
 	if (port == NULL) return true;
 
@@ -106,6 +123,95 @@ bool valid_port(char* port)
 void usage(char *name)
 {
 	fprintf(stderr, "usage: %s -u <username> -i <ip> [-p <port>]\n", name);
+}
+
+
+int connect_to_server(char *ip, char *uname, char *port)
+{
+	/* figure out port number */
+	int iport = 420; // default
+	if (port != NULL)
+	{
+		iport = strtol(port, NULL, 10);
+	}
+
+	/* set up server address */
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(port);
+	server_address.sin_addr.s_addr = inet_addr(ip);
+
+	/* create socket */
+	int sock;
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		perror("connect_to_server()");
+		return -1;
+	}
+
+	/* connect to server */
+	if (connect(sock, (struct sockaddr *) &server_address, sizeof(struct sockaddr)) == -1)
+	{
+		perror("connect_to_server()");
+		return -1;
+	}
+
+	return sock;
+}
+
+
+int handshake(rsa_t pubkey, rsa_t privkey, rsa_t servkey)
+{
+	/* prepare client info */
+	char* template = "UNAME: user1\nBASE: 62\nEXP: %s\nDIV: %s\n";
+	char handshake[MAX_MSG_LEN];
+	sprintf(handshake, template, pubkey->e, pubkey->d);
+
+	/* send info */
+	send_message(sock, handshake, strlen(handshake));
+
+	/* get server response */
+	if (receive_encrypted_message(sock, handshake, MAX_MSG_LEN, priv) == -1)
+	{
+		fprintf(stderr, "handshake(): failed on call to receive_encrypted_message()\n");
+		return -1;
+	}
+
+	/* parse server info */
+	char *field_ptr;
+	char parse_buff[MAX_MSG_LEN];
+	int len;
+
+	if ((field_ptr = strstr(handshake, "BASE: ")) == NULL)
+	{
+		fprintf(stderr, "handshake(): server response missing BASE field\n");
+		return -1;
+	}
+	sscanf(field_ptr, "BASE: %d\n", &servkey->b);
+
+	if ((field_ptr = strstr(handshake, "EXP: ")) == NULL)
+	{
+		fprintf(stderr, "handshake(): server response missing EXP field\n");
+		return -1;
+	}
+	sscanf(field_ptr, "EXP: %s\n", parse_buff);
+	len = strlen(parse_buff);
+	servkey->e = malloc(len + 1);
+	memcpy(servkey->e, parse_buff, len);
+	servkey->e[len] = '\0';
+
+	if ((field_ptr = strstr(handshake, "DIV: ")) == NULL)
+	{
+		fprintf(stderr, "handshake(): server response missing DIV field\n");
+		return -1;
+	}
+	sscanf(field_ptr, "DIV: %s\n", parse_buff);
+	len = strlen(parse_buff);
+	servkey->d = malloc(len + 1);
+	memcpy(servkey->d, parse_buff, len);
+	servkey->d[len] = '\0';
+
+	return 0;
 }
 
 
